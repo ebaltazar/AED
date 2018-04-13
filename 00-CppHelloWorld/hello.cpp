6534af63c41e12ca41fbba74dbb5c9420e7a59f4
ebaltazar <38117838+ebaltazar@users.noreply.github.com>
2018-04-13 13:06:08 -0300

    Update hello.cpp

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+int main()
+{
+	std::cout <<"Hello World\n";
+	std::cout <<"Bye World" << std::endl;
+}
+/*
+// using namespace std; // para evitar colocar std::cout siempre
+*/
