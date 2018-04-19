533158671c2191c23788db7f90a625ba28d13381
ebaltazar <38117838+ebaltazar@users.noreply.github.com>
2018-04-19 00:37:40 -0300

    Add files via upload

---
+++ 01-Adicion/Adicion.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+	int a;				//Declaro un numero
+	std::cin >> a;		//Leo numero
+	int b;				//Declaro otro numero
+	std::cin >> b;		//Leo numero
+	std::cout << a+b;	//Devuelvo la suma
+}
